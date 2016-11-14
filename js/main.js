2ad90b51f688dbe8abc6f3ccd285b2e960971545
inwisloklem <89099109104@mail.ru>
2016-11-14 13:12:02 +0300

    Rearrange js code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var searchFormBtn = document.querySelector(".search-form-btn");
 var modalForm = document.querySelector(".modal-form");
+var counterLess = modalForm.querySelectorAll(".form-counter-less");
+var counterMore = modalForm.querySelectorAll(".form-counter-more");
+var inputs = modalForm.querySelectorAll(".form-counter-wrapper input");
 
 modalForm.classList.add("modal-form-hide");
 
@@ -9,9 +12,13 @@ searchFormBtn.addEventListener("click", function(event) {
   modalForm.classList.toggle("modal-form-hide");
 });
 
-var counterLess = modalForm.querySelectorAll(".form-counter-less");
-var counterMore = modalForm.querySelectorAll(".form-counter-more");
-var inputs = modalForm.querySelectorAll(".form-counter-wrapper input");
+modalForm.addEventListener("submit", function(event) {
+  if(inputs[0].value < 1) {
+    event.preventDefault();
+
+    alert("Необходимо выбрать минимум одного взрослого.")
+  }
+});
 
 for (var i = 0; i < inputs.length; i++) {
   (function(i) {
@@ -29,14 +36,6 @@ for (var i = 0; i < inputs.length; i++) {
     });
   }(i));
 }
-
-modalForm.addEventListener("submit", function(event) {
-  if(inputs[0].value < 1) {
-    event.preventDefault();
-
-    alert("Необходимо выбрать минимум одного взрослого.")
-  }
-});
 
 function initMap() {
   var locationSedona = {lat: 34.865969, lng: -111.763604};
